a8f1ca39c9ef407dfa0b621c3d19b4a24104b9c8
AndreLuz <th3lightman@gmail.com>
2016-11-13 20:21:12 +0000

    album page, tracks

--- model/Track.js
+++ model/Track.js
@@ -4,7 +4,6 @@ function Track(obj){
     this.name = obj.name
     this.track_number = obj.track_number
     this.artists = obj.artists
-    this.duration = (obj.duration_ms /60000).toFixed(2)
 }
 
 module.exports = Track
